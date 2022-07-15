8c5aa603dcf05c42932d3275ffdd51a76cc77f76
João Vitor Gomes Lara Resende <joaovitorglr@gmail.com>
2022-07-14 22:08:22 -0300

    Entrega do exercício de Thinking React

---
+++ modulo3/thinking-react/MockDeDados.js
@@ -0,0 +1,8 @@
+const produtos = [
+    {id:1, nomeDoProduto:"produto1", precoDoProduto:"50", imagemDoProduto:"https://picsum.photos/id/1/200/300"},
+    {id:2, nomeDoProduto:"produto2", precoDoProduto:"100", imagemDoProduto:"https://picsum.photos/id/2/200/300"},
+    {id:3, nomeDoProduto:"produto3", precoDoProduto:"200", imagemDoProduto:"https://picsum.photos/id/3/200/300"},
+    {id:4, nomeDoProduto:"produto4", precoDoProduto:"250", imagemDoProduto:"https://picsum.photos/id/4/200/300"},
+    {id:5, nomeDoProduto:"produto5", precoDoProduto:"300", imagemDoProduto:"https://picsum.photos/id/5/200/300"},
+    {id:6, nomeDoProduto:"produto6", precoDoProduto:"400", imagemDoProduto:"https://picsum.photos/id/6/200/300"}
+]

f22867d699b4e502a2247d9f41e3bc131d6ae55f
João Vitor Gomes Lara Resende <joaovitorglr@gmail.com>
2023-03-23 16:42:35 -0300

    Entrega dos exercícios de complexidade de algoritmos

---
+++ modulo7/complexidade-de-algoritmos/src/exercise1/index.ts
@@ -0,0 +1,12 @@
+const findFirstRecurringCharacter = (input) => {
+   const charHashMap = {};
+   for (const char of input) {
+     if (charHashMap[char] === true) {
+       return char;
+     }
+     charHashMap[char] = true;
+   }
+   return null;
+ }; 
+
+// O(n)
